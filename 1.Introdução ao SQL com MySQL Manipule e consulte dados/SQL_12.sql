bc1ca19742c1ef5fa101d018d206e83afce777bf
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-11-10 20:26:42 -0300

    sql

--- 1.Introdução ao SQL com MySQL Manipule e consulte dados/SQL_12.sql
+++ 1.Introdução ao SQL com MySQL Manipule e consulte dados/SQL_12.sql
@@ -1,8 +1,9 @@
-
+/*SELEÇÃO DOS DADOS DA TABELA*/
 SELECT * FROM tbproduto WHERE PRODUTO = '544931';
 
 SELECT * FROM tbcliente WHERE CIDADE = 'Rio de Janeiro';
 
 SELECT * FROM tbproduto WHERE SABOR = 'Cítricos';
 
+/*ATUALIZAÇÃO DOS DADOS NA TABELA*/
 UPDATE tbproduto SET SABOR = 'Cítricos' WHERE SABOR = 'Limão';
--- 1.Introdução ao SQL com MySQL Manipule e consulte dados/SQL_14.sql
+++ 1.Introdução ao SQL com MySQL Manipule e consulte dados/SQL_14.sql
@@ -1,3 +1,4 @@
+/*SELEÇÃO DOS DADOS DA TABELA*/
 
 SELECT * FROM tbcliente;
 
