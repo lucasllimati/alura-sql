51b1905763bed556c01baa3115bf75608a1e541a
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-12-20 08:44:54 -0300

    sql

---
+++ 4.SQL Server Consultas avançadas com Microsoft SQL Server 2017/2.Consultas Condicionais.sql
@@ -0,0 +1,41 @@
+/* Consultas condicionais*/
+
+USE SUCOS_VENDAS
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] = 'Manga' OR [TAMANHO] = '700 ml' 
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] = 'Manga' AND [TAMANHO] = '700 ml' 
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+NOT ([SABOR] = 'Manga' AND [TAMANHO] = '700 ml')
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+NOT ([SABOR] = 'Manga' OR [TAMANHO] = '700 ml')
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] = 'Manga' AND NOT ( [TAMANHO] = '700 ml')
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] IN ('Manga', 'Laranja')
+
+/* Semelhante a consulta acima utilizando o IN */
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] = 'Manga' OR [SABOR] ='Laranja'
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] NOT IN ('Manga', 'Laranja')
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+NOT ([SABOR] = 'Manga' OR [SABOR] ='Laranja')
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] IN ('Manga', 'Laranja') AND [PREÇO DE LISTA] > 10
+
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] IN ('Manga', 'Laranja') AND [PREÇO DE LISTA] BETWEEN 10 AND 13
+
+/* Semelhante a consulta acima utilizando o BETWEEN */
+SELECT * FROM [TABELA DE PRODUTOS] WHERE 
+[SABOR] IN ('Manga', 'Laranja') AND [PREÇO DE LISTA] >= 10 AND [PREÇO DE LISTA] <= 13
