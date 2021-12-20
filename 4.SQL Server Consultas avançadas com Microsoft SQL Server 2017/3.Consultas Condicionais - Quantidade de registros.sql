509c91c37769b9de22d237783dc57bbbc9ff86d3
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-12-20 08:56:13 -0300

    sql

---
+++ 4.SQL Server Consultas avançadas com Microsoft SQL Server 2017/3.Consultas Condicionais - Quantidade de registros.sql
@@ -0,0 +1,6 @@
+/* Consulta condicional - Quantidade de registros */
+
+/* Observe no canto inferior direito do grid, que apresenta o resultado o número de registros listados. Quantos registros foram retornados? 1932 */
+
+SELECT *  FROM [ITENS NOTAS FISCAIS] 
+WHERE [QUANTIDADE] > 60 AND [PREÇO] <= 3
---
+++ 4.SQL Server Consultas avançadas com Microsoft SQL Server 2017/5.Consultas Condicionais - Buscando clientes.sql
@@ -0,0 +1,5 @@
+/* Consultas Condicionais - Buscando clientes */
+/* Quantos clientes possuem o último sobrenome Mattos? 2 */
+
+SELECT * FROM [TABELA DE CLIENTES] WHERE
+NOME LIKE '%Mattos'
