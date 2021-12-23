d8a11d21ee1331db1c67dfa5e6116e53f4b3ae92
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2021-12-23 08:58:15 -0300

    sql

---
+++ 4.SQL Server Consultas avançadas com Microsoft SQL Server 2017/17.Consultas - Classificação de clientes.sql
@@ -0,0 +1,14 @@
+/* 17.Consultas - Classificação de clientes */
+/* 
+Veja o ano de nascimento dos clientes e classifique-os como: nascidos antes de 1990 são adultos, 
+nascidos entre 1990 e 1995 são jovens e nascidos depois de 1995 são crianças. Liste o nome do cliente e esta classificação. 
+*/
+
+USE SUCOS_VENDAS
+
+SELECT [NOME],
+CASE 
+    WHEN YEAR([DATA DE NASCIMENTO]) < 1990 THEN 'Adulto'
+    WHEN YEAR([DATA DE NASCIMENTO]) BETWEEN 1990 AND 1995 THEN 'Jovem'
+    ELSE 'Criança' END AS [Classificação Etária]
+ FROM [TABELA DE CLIENTES]
