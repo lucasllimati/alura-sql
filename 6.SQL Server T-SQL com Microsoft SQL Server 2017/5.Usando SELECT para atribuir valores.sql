1514a9074a7c34c15655b252badb73885f456898
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2022-04-01 21:46:42 -0300

    sql

---
+++ 6.SQL Server T-SQL com Microsoft SQL Server 2017/5.Usando SELECT para atribuir valores.sql
@@ -0,0 +1,11 @@
+-- Usando SELECT para atribuir valores
+
+/*
+Crie uma variável chamada NUMNOTAS e atribua a ela o número de notas fiscais do dia 01/01/2017. Mostre na saída do script o valor da variável.
+*/
+
+DECLARE @NUMNOTAS INTEGER
+
+SELECT @NUMNOTAS = COUNT(*) FROM [NOTAS FISCAIS] 
+    WHERE DATA = '20170101'
+PRINT @NUMNOTAS
