be0f6845405f5178f82fe5ae0e5985ce0766ec17
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2022-10-04 22:01:17 -0300

    Update 47.Calculando o valor total do faturamento.sql

--- 6.SQL Server T-SQL com Microsoft SQL Server 2017/47.Calculando o valor total do faturamento.sql
+++ 6.SQL Server T-SQL com Microsoft SQL Server 2017/47.Calculando o valor total do faturamento.sql
@@ -1,30 +1,41 @@
--- Achando o valor total do crédito
+-- Calculando o valor total do faturamento
 
 USE [SUCOS_VENDAS]
 
 /*
-Crie um script usando um cursor para achar o valor total de todos os créditos de todos os clientes.
+Crie um script usando um cursor para achar o valor total do faturamento para um mês e um ano.
 
-Dicas: Declare duas variáveis: uma que receba o limite de crédito do cursor e outra o limite de crédito total;
-Faça um loop no cursor e vá somando o valor do limite de cada cliente na variável de limite de crédito total;
-Exiba o valor total do limite.
-
-Um exemplo de script é:
+Dicas:
+Declare três variáveis: uma que recebe a quantidade, outra o preço e uma terceira que irá acumular o faturamento;
+Faça um loop no cursor e vá somando o faturamento de cada nota;
+Exiba o valor total do limite;
+Lembrando a consulta que obtém o faturamento dentro de um mês e ano:
 */
 
-DECLARE @LIMITECREDITO FLOAT
-DECLARE @LIMITECREDITOACUM FLOAT
-DECLARE CURSOR1 CURSOR FOR SELECT [LIMITE DE CREDITO] 
-    FROM [TABELA DE CLIENTES]
-SET @LIMITECREDITOACUM = 0
+SELECT INF.QUANTIDADE, INF.PREÇO FROM [ITENS NOTAS FISCAIS] INF
+INNER JOIN [NOTAS FISCAIS] NF ON NF.NUMERO = INF.NUMERO
+WHERE MONTH(NF.DATA) = 1 AND YEAR(NF.DATA) = 2017
+
+-- Um exemplo de script é:
+DECLARE @QUANTIDADE INT
+DECLARE @PRECO FLOAT
+DECLARE @FATURAMENTOACUM FLOAT
+DECLARE @MES INT
+DECLARE @ANO INT
+SET @MES = 1
+SET @ANO = 2017
+DECLARE CURSOR1 CURSOR FOR SELECT INF.QUANTIDADE, INF.PREÇO FROM 
+    [ITENS NOTAS FISCAIS] INF
+INNER JOIN [NOTAS FISCAIS] NF ON NF.NUMERO = INF.NUMERO
+WHERE MONTH(NF.DATA) = @MES AND YEAR(NF.DATA) = @ANO
+SET @FATURAMENTOACUM = 0
 OPEN CURSOR1
-FETCH NEXT FROM CURSOR1 INTO @LIMITECREDITO
+FETCH NEXT FROM CURSOR1 INTO @QUANTIDADE, @PRECO
 WHILE @@FETCH_STATUS = 0
 BEGIN
-    SET @LIMITECREDITOACUM = @LIMITECREDITOACUM + 
-        @LIMITECREDITO
-    FETCH NEXT FROM CURSOR1 INTO @LIMITECREDITO
+    SET @FATURAMENTOACUM = @FATURAMENTOACUM + (@QUANTIDADE * @PRECO)
+    FETCH NEXT FROM CURSOR1 INTO @QUANTIDADE, @PRECO
 END
 CLOSE CURSOR1
 DEALLOCATE CURSOR1
-PRINT @LIMITECREDITOACUM
+PRINT @FATURAMENTOACUM
