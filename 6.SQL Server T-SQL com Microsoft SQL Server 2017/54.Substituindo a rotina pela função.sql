f5244decd0ad7d138438bffcedebd76b6197b8cd
Lucas Lima <69853124+lucasllimati@users.noreply.github.com>
2022-10-04 22:01:41 -0300

    Create 54.Substituindo a rotina pela função.sql

---
+++ 6.SQL Server T-SQL com Microsoft SQL Server 2017/54.Substituindo a rotina pela função.sql
@@ -0,0 +1,16 @@
+-- Criando função para obter dados das entidades
+
+USE [SUCOS_VENDAS]
+
+-- CLIENTE ALEATORIO
+DECLARE @CLIENTE VARCHAR(20)
+DECLARE @VENDEDOR VARCHAR(20)
+DECLARE @PRODUTO VARCHAR(20)
+
+SET @CLIENTE = [dbo].[EntidadeAleatoria]('CLIENTE')
+SET @VENDEDOR = [dbo].[EntidadeAleatoria]('VENDEDOR')
+SET @PRODUTO = [dbo].[EntidadeAleatoria]('PRODUTO')
+
+PRINT @VENDEDOR
+PRINT @CLIENTE
+PRINT @PRODUTO
